97848aaf72ac125958a4a5f630dcd9efef9359f4
Pierre Lestringant <pierre.lestringant@gmail.com>
2023-09-27 21:59:58 +0200

    [Setup][Minor] typo in comment

--- setup/profile/tex/user.sh
+++ setup/profile/tex/user.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Execute as root to install TEX from REF after root.sh
+# Execute as user to install TEX from REF after root.sh
 
 ln -s /usr/share/myspell/dicts/* /home/user/.config/Code/Dictionaries
