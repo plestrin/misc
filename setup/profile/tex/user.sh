f0c0ef1813119a1d31ef6d52f543c73e569347e4
Pierre Lestringant <pierre.lestringant@gmail.com>
2023-09-27 22:35:21 +0200

    [Setup][Minor] check user script run as user

--- setup/profile/tex/user.sh
+++ setup/profile/tex/user.sh
@@ -2,4 +2,9 @@
 
 # Execute as user to install TEX from REF after root.sh
 
+if [ "$(id -u)" -eq 0 ]; then
+    echo "[-] this script must be run as simple user"
+    exit 1
+fi
+
 ln -s /usr/share/myspell/dicts/* /home/user/.config/Code/Dictionaries
