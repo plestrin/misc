4c6ec5f20306b44436d90855d04abdc87a59101d
Pierre Lestringant <pierre.lestringant@gmail.com>
2023-10-01 18:18:58 +0200

    [Setup][Minor] improvements
    * more robust sourcing ;
    * premature exit on error.

--- setup/profile/tex/root.sh
+++ setup/profile/tex/root.sh
@@ -5,14 +5,24 @@
 SCRIPT_PATH=$(cd -- "$(dirname "$0")" > /dev/null 2>&1; pwd -P)
 ROOT_PATH=${SCRIPT_PATH}/../../
 
-source "${ROOT_PATH}shared_root.sh"
+source "${ROOT_PATH}shared_root.sh" || exit 1
 
-hostnamectl set-hostname "tex-deb${VERSION}-64"
+hostnamectl set-hostname "tex-deb${VERSION}-64" || exit 1
 
-apt-get update
-apt-get dist-upgrade -y
-apt-get install -y evince apt-transport-https gpg texlive texlive-lang-french texlive-latex-extra texlive-fonts-extra git hunspell-en-us hunspell-fr
+apt-get update || exit 1
+apt-get dist-upgrade -y || exit 1
+apt-get install -y      \
+    evince              \
+    apt-transport-https \
+    gpg                 \
+    texlive             \
+    texlive-lang-french \
+    texlive-latex-extra \
+    texlive-fonts-extra \
+    git                 \
+    hunspell-en-us      \
+    hunspell-fr || exit 1
 
-setup_root_vscode
+setup_root_vscode || exit 1
 
-setup_root_motd "TEX DEB${VERSION}-64"
+setup_root_motd "TEX DEB${VERSION}-64" || exit 1
